e1d80425d0558422e3485cc765e013bd81ba96a4
Santo2711 <santodevil2711@gmail.com>
2025-12-18 17:30:11 +0530

    Suppliers

--- src/app/suppliers/suppliers.ts
+++ src/app/suppliers/suppliers.ts
@@ -1,11 +1,50 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-suppliers',
-  imports: [],
+  imports: [FormsModule, CommonModule, RouterOutlet],
   templateUrl: './suppliers.html',
   styleUrl: './suppliers.css',
 })
 export class Suppliers {
+
+  searchText='';
+
+  suppliers=[
+    {
+      name:"Supplier 1",
+      city:"Chennai",
+      state:"Tamil Nadu",
+      phone:"+91 xxxxx xxxxx",
+      status:"Active",
+      linkedhubs:[
+        {name:"Hub 1", city:"Chennai"},
+        {name:"Hub 2", city:"Chennai"}
+      ],
+    },
+    {
+      name:"Supplier 2",
+      city:"Bengaluru",
+      state:"Karnataka",
+      phone:"+91 xxxxx xxxxx",
+      status:"Inactive",
+      linkedhubs:[
+        {name:"Hub 1", city:"Bengaluru"},
+        {name:"Hub 2", city:"Bengaluru"}
+      ],
+    }
+  ];
+
+  filteredsuppliers(){
+    return this.suppliers.filter(supplier=>
+      supplier.name.toLowerCase().includes(this.searchText.toLowerCase())||
+      supplier.city.toLowerCase().includes(this.searchText.toLowerCase())
+    );
+  }
+
+ 
 
 }
